ec72abee7a3de3748aa9126d279a344d99cecb72
Adrian Fazio <adrian.fazio@outlook.com>
2020-03-14 11:44:00 -0300

    variantes final

--- variantes.js
+++ variantes.js
@@ -5,14 +5,14 @@ function traerMuseosDeLaAPI() {
   superagent
     .get("https://www.cultura.gob.ar/api/v2.0/museos")
     .query({ format: "json", limit: 5 })
-    .end(escribirMuseosEnArchivo);
+    .end(escribirMuseosEnArchivo, console.log('Bajando museos'));
 }
 
 function traerOrganismosDeLaAPI() {
   superagent
     .get("https://www.cultura.gob.ar/api/v2.0/organismos")
     .query({ format: "json", limit: 10 })
-    .end(escribirOrganismosEnArchivo);
+    .end(escribirOrganismosEnArchivo, console.log('Bajando organismos'));
 }
 
 function escribirMuseosEnArchivo(error, respuesta) {
@@ -27,21 +27,22 @@ function escribirMuseosEnArchivo(error, respuesta) {
 
   for (var i=0; i<museos.length-1; i++) {
     fs.appendFile("variantes.txt", 
-    "Museo: " +  
-    museos[i].nombre+" (" +
-    museos[i].direccion + ")"+
-    ". Por cualquier consulta comunicarse al " + 
-    museos[i].telefono+"\n",avisarQueTerminamos);
+    "Museo: " + museos[i].nombre+" (" + museos[i].direccion + ")"+
+    ". Por cualquier consulta comunicarse al " + museos[i].telefono+"\n", 
+    avisarQueEscribeMuseo);
   }
   var i=museos.length-1;
   fs.appendFile("variantes.txt", 
-  "Museo: " +  
-  museos[i].nombre+" (" +
-  museos[i].direccion + ")"+
-  ". Por cualquier consulta comunicarse al " + 
-  museos[i].telefono+"\n",traerOrganismosDeLaAPI);
+  "Museo: " +  museos[i].nombre+" (" + museos[i].direccion + ")"+
+  ". Por cualquier consulta comunicarse al " + museos[i].telefono+"\n",
+  traerOrganismosDeLaAPI);
+}
 
-
+function avisarQueEscribeMuseo(aviso){
+  if (aviso) {
+    throw aviso;
+  }
+  console.log('Escribiendo museo en archivo')
 }
 
 function escribirOrganismosEnArchivo(error, respuesta) {
@@ -54,12 +55,17 @@ function escribirOrganismosEnArchivo(error, respuesta) {
   console.log(`Se encontraron ${cantidad} Organismos. Escribiendo en archivo...`);
   for (var i=0; i<organismos.length; i++) {
     fs.appendFile("variantes.txt", 
-    "Organismo: " +  
-    organismos[i].nombre+" (" +
-    organismos[i].direccion + ")"+
-    ". Por cualquier consulta comunicarse al " + 
-    organismos[i].telefono+"\n",avisarQueTerminamos);
+    "Organismo: " + organismos[i].nombre+" (" + organismos[i].direccion + ")"+
+    ". Por cualquier consulta comunicarse al " + organismos[i].telefono+"\n",
+    avisarQueEscribeOrganismo);
   }
+}
+
+function avisarQueEscribeOrganismo(aviso){
+  if (aviso) {
+    throw aviso;
+  }
+  console.log('Escribiendo organismo en archivo')
 }
 
 function avisarQueTerminamos(error) {
